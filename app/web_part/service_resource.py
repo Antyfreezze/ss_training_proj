5c0fc0ce69891b3b356138448e964fdcc17fbbab
Antyfreezze <neggrr@gmail.com>
2018-06-07 13:52:33 +0300

    homework

--- app/web_part/service_resource.py
+++ app/web_part/service_resource.py
@@ -1,23 +1,34 @@
 from sanic.response import json, text
-from processor.authorization import tokenize
+from app.processor.authorization import tokenize, token_checker
+from app.database import (delete_entry, upsert_entry,
+    get_entry)
+
 
 async def smoke(request):
-    return text('hello, world!')
+    return text('Hello')
 
 
 # TODO: get token via jwt
 async def login(request):
     token = await tokenize({request.args.get('user'): request.args.get('pass')})
-    return text(token)
+    response = text('')
+    response.cookies['token'] = token
+    response.cookies['token']['max-age'] = 20
+    test_cookie = response.cookies.get('session')
+    return text("Test cookie set to: {}".format(test_cookie))
 
 
 # TODO: add functions for comunicate with DB
 async def project(request):
     if request.method == 'GET':
+        await get_entry(table, id)
         return text('this is project with GET')
     if request.method == 'POST':
+        await upsert_entry(table_name, id)
         return text('this is project with POST')
     if request.method == 'PUT':
+        await upsert_entry(table_name, id)
         return text('this is project with PUT')
     if request.method == 'DELETE':
+        await delete_entry(table_name, id)
         return text('this is project with DELETE')
--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 from app import app
-from config import web
+from app.config import web
+
+
+# @app.listener('before_server_start')
+# async def setup_db(app, loop):
+#     app.db = await db_setup()
+
 
 if __name__ == '__main__':
     app.app.run(host=web['host'], port=web['port'], debug=web['debug'])
