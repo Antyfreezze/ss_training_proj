94dfb2c688b11e882bb01dd084651706172cf701
Antyfreezze <neggrr@gmail.com>
2018-06-15 16:29:37 +0300

    fixed tests to asynctest

--- test/login_test.py
+++ test/login_test.py
@@ -1,32 +1,33 @@
-from sanic import Sanic
-from app.resources.login_view import LoginView
-
-app = Sanic('test_login')
-
-app.add_route(LoginView.as_view(), '/login')
+from app.app import app
+from asynctest import TestCase
 
 
-def test_login_get_not_alowed():
-    request, response = app.test_client.get('/login')
-    assert response.status == 405
+class TestLogin(TestCase):
+
+    def create_app(self):
+        return app
+
+    def test_login_get_not_alowed(self):
+        request, response = app.test_client.get('/login')
+        self.assertEqual(response.status, 405)
 
 
-def test_login_put_not_alowed():
-    request, response = app.test_client.put('/login')
-    assert response.status == 405
+    def test_login_put_not_alowed(self):
+        request, response = app.test_client.put('/login')
+        self.assertEqual(response.status, 405)
 
 
-def test_login_delete_not_alowed():
-    request, response = app.test_client.delete('/login')
-    assert response.status == 405
+    def test_login_delete_not_alowed(self):
+        request, response = app.test_client.delete('/login')
+        self.assertEqual(response.status, 405)
 
 
-def test_login_post_without_params_not_alowed():
-    request, response = app.test_client.post('/login')
-    assert response.status == 422
+    def test_login_post_without_params_not_alowed(self):
+        request, response = app.test_client.post('/login')
+        self.assertEqual(response.status, 422)
 
 
-def test_login_post_valid_params():
-    params = {"login":"vasya", "password":"password"}
-    request, response = app.test_client.post('/login', params=params)
-    assert response.status == 200
+    def test_login_post_valid_params(self):
+        params = {"login":"vasya", "password":"password"}
+        request, response = app.test_client.post('/login', data=params)
+        self.assertEqual(response.status, 200)
---
+++ test/project_test.py
@@ -0,0 +1,26 @@
+from app.app import app
+from asynctest import TestCase
+
+class TestProject(TestCase):
+    def test_project_get_without_id(self):
+        params = {'user_id':'1'}
+        request, response = app.test_client.get('/projects/', params=params)
+        self.assertEqual(response.status, 200)
+
+
+    def test_project_get_with_valid_id(self):
+        params = {'user_id':'1'}
+        request, response = app.test_client.get('/projects/3', params=params)
+        self.assertEqual(response.status, 200)
+
+
+
+    def test_project_get_id_not_found(self):
+        request, response = app.test_client.get('/projects/2123123')
+        self.assertEqual(response.status, 500)
+
+
+
+    def test_project_get_invalid_id(self):
+        request, response = app.test_client.get('/projects/asdaw')
+        self.assertEqual(response.status, 500)
